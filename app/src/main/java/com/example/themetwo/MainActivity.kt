88e3f52e8a63689488da114b6be051d776bb1f4b
Svetlana184 <sveta.kgd.39@gmail.com>
2025-11-26 14:52:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/themetwo/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.themetwo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.themetwo.ui.theme.ThemeTwoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            //1 вариант
+            //var count = remember { mutableStateOf(0) }
+            //2 вариант
+            var count by remember { mutableStateOf(0) }
+            Box(contentAlignment = Alignment.Center){
+                Column() {
+                    Text("Clicks : ${count}", fontSize = 28.sp)
+                    Button(onClick = {count++}) {
+                        Text("Click me!")
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ThemeTwoTheme {
+        Greeting("Android")
+    }
+}
